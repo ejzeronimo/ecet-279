19e7a5c882a2ca537b98994932ca14846147d736
Ethan Zeronik <ejzeronik@gmail.com>
2022-08-24 14:48:40 -0400

    finished lab one, lab 2 is left

--- Lab_One/Procedure_One/Lab_One/main.c
+++ Lab_One/Procedure_One/Lab_One/main.c
@@ -5,21 +5,43 @@
  * Created: 8/24/2022 2:09:46 PM
  * Author: Ethan Zeronik
  *
- * Operations: turn on onboard LED every second
+ * Operations: turn on board LED every second
  *
  * Hardware:
  *   Atmega2560          micro controller
- *   PORTx.x             LED13 active high
+ *   PORTD.x             LED13 active high
  */
 
 #include <avr/io.h>
 
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+/* NOTE: Function prototypes */
+
+// inits IO ports
+void io_init(void);
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
+    io_init();
+
+    while(1)
     {
+        // DEBUG: comment this line out when using simulator
+        _delay_ms(500);
+
+        // set it to the inverse of the current value
+        PORTB = ~(0x80 & PORTB);
     }
 }
 
+/* NOTE: Function implementations */
+void io_init(void)
+{
+    // set led 13 as output
+    DDRB  = 0x80;
+    // turn off led on init
+    PORTB = 0x80;
+}
