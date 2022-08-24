3d1e466b9e228d5f28b348c7b442128fec889639
Ethan Zeronik <ejzeronik@gmail.com>
2022-08-24 14:16:00 -0400

    proc one init

---
+++ Lab_One/Procedure_One/Lab_One/main.c
@@ -0,0 +1,25 @@
+/*
+ * FileName: main.c
+ * Version: 1
+ *
+ * Created: 8/24/2022 2:09:46 PM
+ * Author: Ethan Zeronik
+ *
+ * Operations: turn on onboard LED every second
+ *
+ * Hardware:
+ *   Atmega2560          micro controller
+ *   PORTx.x             LED13 active high
+ */
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
