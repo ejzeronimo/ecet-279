4560a2c96b6c1d62016383a89e25afaa92d0a13f
Ethan Zeronik <ejzeronik@gmail.com>
2022-09-07 12:52:18 -0400

    checkpoint commit

---
+++ Lab Template/main.c
@@ -0,0 +1,22 @@
+/*
+ * $safeprojectname$.c
+ *
+ * Created: 9/7/2022 12:00:50 PM
+ * Author : Ethan Zeronik
+ */ 
+
+#include <avr/io.h>
+
+#include "Debugger.h"
+
+
+int main(void)
+{
+	initDebug();
+	
+	
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
---
+++ Lab Two/Procedure One/main.c
@@ -0,0 +1,22 @@
+/*
+ * Procedure One.c
+ *
+ * Created: 9/7/2022 12:00:50 PM
+ * Author : Ethan Zeronik
+ */ 
+
+#include <avr/io.h>
+
+#include "Debugger.h"
+
+
+int main(void)
+{
+	initDebug();
+	
+	
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
